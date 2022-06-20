893f9043dbf375a05fb39d0b2aaf474435ea4424
71200675sabrina <79124326+71200675sabrina@users.noreply.github.com>
2022-06-20 17:44:42 +0700

    Add files via upload

---
+++ TokoMonster/src/main/java/com/uas/prakrplbo/Monster.java
@@ -0,0 +1,32 @@
+package com.uas.prakrplbo;
+
+public abstract class Monster implements MonsterInterface{
+    private String nama;
+
+    public abstract String bersuara();
+
+    public Monster(String nama){
+        this.nama = nama;
+    }
+
+    public void getInfo(){
+        System.out.println("Nama:" + getNama());
+        System.out.println("Suara : " + bersuara());
+        String gerakan ="";
+        if (this instanceof Manticore){
+            gerakan ="Terbang";
+            System.out.println("Gerak: " +getNama() + " " + gerakan + "....");
+        } else if(this instanceof Cyclops){
+            gerakan =" Berjalan";
+            System.out.println("Gerak" + getNama()+ " " + gerakan + "...." );
+        }else if(this instanceof Cerberus){
+            gerakan =" Berlari";
+            System.out.println("Gerak" + getNama()+ " " + gerakan + "...." );
+        }
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+
+}
